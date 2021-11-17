fac62ee0c6fabd94f0016e625075a08bc5c270bc
Semih Çağlar <semihcaglar28@gmail.com>
2021-11-17 15:11:16 +0300

    Compress fonksiyonu yazıldı.

--- Codes/indirme.cpp
+++ Codes/indirme.cpp
@@ -1,67 +1,75 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-const int N=1e3;
-const int indirgenecekSayi=10;
 typedef pair<double,double> dd;
-dd dizi[N];
-int n;
-double xgenislik;
-double ans[indirgenecekSayi];
-bool bulamadik[indirgenecekSayi];
+int compress_size=20;
+double width;
 
-int main(){
-    freopen("/home/semih/Desktop/Proj/test/test.in","r",stdin);
-    freopen("/home/semih/Desktop/Proj/test/test.out","w",stdout);
-    cin>>n;
-    for(int i=0;i<n;++i){
-        double x,y;
-        cin>>x>>y;
-        dizi[i]={x,y};        
-    }
-    sort(dizi,dizi+n);
-    xgenislik=(dizi[n-1].first-dizi[0].first)/indirgenecekSayi;
+vector<double> compress(vector<dd> ar){
+    if(ar.size()<compress_size)
+        compress_size=ar.size();
 
-    double sol=dizi[0].first, sag=sol+xgenislik, sum=0;
-    int cnt=0, aralik=0, ind=0;
+    sort(ar.begin(),ar.end());
+    width=(ar.rbegin()->first - ar.begin()->first)/compress_size;
 
-    while(ind<n){
-        if(dizi[ind].first>sag){
-            sol=sag;
-            sag=sol+xgenislik;
+    double left=ar.begin()->first, right=left+width, sum=0;
+    int cnt=0,range=0,ind=0;
+
+    vector<bool> not_found(ar.size(),false);
+    vector<double> ans(compress_size);
+
+    while(ind<ar.size()){
+        if(ar[ind].first>right){
+            left=right;
+            right=left+width;
             if(cnt==0)
-                bulamadik[aralik]=true;
+                not_found[range]=true;
             else
-                ans[aralik]=sum/cnt;
+                ans[range]=sum/cnt;
             sum=0;
             cnt=0;
-            aralik++;
+            range++;
         }
         else{
-            sum+=dizi[ind].second;
+            sum+=ar[ind].second;
             cnt++;
             ind++;
         }
     }
-
     if(cnt==0)
-        bulamadik[aralik]=true;
+        not_found[range]=true;
     else
-        ans[aralik]=sum/cnt;
-
-    int enyakinsag=-1;
-
-    for(int i=1;i+1<indirgenecekSayi;++i){
-        if(bulamadik[i]==true){
-            if(i>enyakinsag){
+        ans[range]=sum/cnt;
+    
+    int near_right=-1;
+    for(int i=1;i+1<compress_size;i++){
+        if(not_found[i]==true){
+            if(i>near_right){
                 int j;
-                for(j=i;j<n and bulamadik[j]==true;++j);
-                enyakinsag=j;
+                for(j=i;j<ar.size() && not_found[j]==true;j++);
+                near_right=j;
             }
-            ans[i]=ans[i-1] + (ans[enyakinsag]-ans[i-1]) / (enyakinsag-i+1) ;
-            bulamadik[i]=false;
+            ans[i] = ans[i-1] + (ans[near_right]-ans[i-1]) / (near_right-i+1) ;
+            not_found[i]=false;
         }
     }
-    for(int i=0;i<indirgenecekSayi;++i)
+
+    return ans;
+}
+
+int main(){
+    freopen("/home/semih/Desktop/Proj/test/test.in","r",stdin);
+    freopen("/home/semih/Desktop/Proj/test/test.out","w",stdout);
+
+    int n;
+    cin>>n;
+    vector<dd> ar(n);
+    for(int i=0;i<n;++i){
+        double x,y;
+        cin>>x>>y;
+        ar[i]={x,y};        
+    }
+    vector<double> ans=compress(ar);
+    for(int i=0;i<compress_size;++i)
         cout<<ans[i]<<"\n";
 }
