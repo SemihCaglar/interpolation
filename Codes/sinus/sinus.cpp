c4c29f973c1649f49d2c24118efb96599a830569
Semih Çağlar <semihcaglar28@gmail.com>
2022-01-25 15:22:06 +0300

    Sinus grafiği çizen kod eklendi.

---
+++ Codes/sinus/sinus.cpp
@@ -0,0 +1,17 @@
+#include<semih/competitive.h>
+
+const double pi=3.14159265359;
+
+int main(){
+    srand(time(NULL));
+    fast;
+    // test();
+
+    cout<<40<<endl;
+    for(int i=0;i<350;++i){        
+        int x=rand()%720-360;
+        cout<<x<<" "<<cos(x*pi/180)<<endl;
+    }
+
+    return 0;
+}
