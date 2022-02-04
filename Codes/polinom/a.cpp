89c06bf70e4e5b8f90ccda39a1aec4b10a1eba3b
Semih Çağlar <semihcaglar28@gmail.com>
2022-02-04 15:51:27 +0300

    Polinom örneği eklendi.

---
+++ Codes/polinom/a.cpp
@@ -0,0 +1,20 @@
+#include<semih/competitive.h>
+
+const double pi=3.14159265359;
+
+int main(){
+    srand(time(NULL));
+    fast;
+    // test();
+
+    cout<<50<<endl;
+    for(int i=0;i<50;++i){        
+        double x=(rand()%400-250)/100.0;
+        // -2,5 ,1,5
+        // -250->150
+        // -3,00 -> 4,00
+        cout<<x<<" "<<2.0*(x-1)*(x+2)*(x)<<endl;
+    }
+
+    return 0;
+}
